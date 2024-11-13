e0ca020005121dcc878dbf4189feef16efd463bb
Sebastian <sebasortega151003@gmail.com>
2024-11-12 22:01:28 -0600

    Agregar archivos de proyecto.

---
+++ ShopEasyApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopEasyApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ShopEasyApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopEasyApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
